0eb8d1b58f5436b01738a4078d800530ee2fcc52
Romeran Rodriguez <romeran14@gmail.com>
2023-12-16 19:29:29 -0800

    fire event

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -3,7 +3,16 @@ import { FirstApp } from "../src/FirstApp"
 
 describe('Pruebas firt app', () => { 
     test('should macth snapshot', () => { 
-        render( <FirstApp></FirstApp>)
+        const title = "hola soy goku"
+        
+        const { container, getByText} = render( <FirstApp title={title}></FirstApp>)
+
+        expect( container).toMatchSnapshot()
+        expect( getByText( title )).toBeTruthy()
+
+        const h1 = container.querySelector('h1')
+        console.log(h1.innerHTML)
+        expect(h1.innerHTML.trim()).toBe(title)
      })
 
  })
